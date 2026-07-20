eb4f49ca6ed8683085e1f209ccd778d289f06f9e
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-07-20 16:01:27 +0530

    create first component

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./Chai"
+
+function App() {
+  const username = "Abhishek Singh"
+
+  return (
+    <>
+      <h1>Chai aur react with vite |  {username}</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
+
+// {username} => evaluated expression - means final outcome // treated as variable
---
+++ basic-react/src/Chai.jsx
@@ -0,0 +1,14 @@
+function Chai(){
+    return(
+        <h3>chai is ready</h3>
+    )
+}
+
+export default Chai
+
+// Function name always start from capital letter
+
+// when making components, always capitalize name(function) eg: Chai.
+// file extension .jsx , bcz some libraries force us for that
+// A component can only return a single element (which may contain multiple child elements and components).
+// So yeah technically, we can return multiple components, but those should be wrapped by a single parent element.
